7f8532f0b9486c46c8c26f7ca346d48285be8c7d
mohit25T <mohittopiya9106@gmail.com>
2026-03-08 17:55:39 +0530

    20

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -34,13 +34,34 @@ function AppRouter() {
 
         {/* Protected Admin Routes */}
         <Route path="/admin/login" element={<Login />} />
-        <Route path="/admin" element={<ProtectedRoute />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/add-product" element={<AddProduct />} />
-          <Route path="/products" element={<ProductsAdmin />} />
-          <Route path="/edit-product/:id" element={<EditProduct />} />
-          <Route path="/leads" element={<Leads />} />
-        </Route>
+          <Route
+            path="/admin/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/admin/add-product" element={
+            <ProtectedRoute>
+              <AddProduct />
+            </ProtectedRoute>
+          } />
+          <Route path="/admin/products" element={
+            <ProtectedRoute>
+              <ProductsAdmin />
+            </ProtectedRoute>
+          } />
+          <Route path="/admin/edit-product/:id" element={
+            <ProtectedRoute>
+              <EditProduct />
+            </ProtectedRoute>
+          } />
+          <Route path="/admin/leads" element={
+            <ProtectedRoute>
+              <Leads />
+            </ProtectedRoute>
+          } />
       </Routes>
     </HashRouter>
   );
